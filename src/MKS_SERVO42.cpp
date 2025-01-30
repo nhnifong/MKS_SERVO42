d75d5decfc5898d904270b4bbec320d36fcbe4b0
Nathaniel Nifong <naavox@gmail.com>
2025-01-29 23:37:37 -0500

    Implement the rest of the serial protocol

--- src/MKS_SERVO42.cpp
+++ src/MKS_SERVO42.cpp
@@ -1,139 +1,302 @@
 #include "MKS_SERVO42.h"
 
-void MKS_SERVO42::initialize(HardwareSerial *serialPort, long const &baudRate)
-{
-	port_ = serialPort;
-	port_->begin(baudRate);
+void MKS_SERVO42::initialize(HardwareSerial *serialPort, long const &baudRate) {
+  port_ = serialPort;
+  port_->begin(baudRate);
 }
 
-bool MKS_SERVO42::ping(byte const &stepperId)
-{
-	// Flush
-	while (port_->read() != -1)
-		;
-	int send = sendMessage(stepperId, instruction::STEPPER_PING);
-	if (send != 3)
-	{
-		Serial.println("Failed to send");
-		return false;
-	}
-	return reciveStepperStatus();
+byte MKS_SERVO42::calculateChecksum(const byte *message, int length) {
+  byte checksum = 0;
+  for (int i = 0; i < length; i++) checksum += message[i];
+  return checksum & 0xFF;
 }
 
-int MKS_SERVO42::sendMessage(byte stepperId, byte const &commandId)
-{
-	stepperId += 0xe0;
-	byte message[3];
-	byte checksum = stepperId + commandId;
-	message[0] = stepperId;
-	message[1] = commandId;
-	message[2] = checksum & 0xFF;
-	for (size_t i = 0; i < 3; i++)
-	{
-		Serial.print(" ");
-		Serial.print(message[i], HEX);
-	}
-	Serial.println();
-	return port_->write(message, 3);
+bool MKS_SERVO42::sendMessage(byte stepperId, byte const &commandId) {
+  // Flush
+  while (port_->read() != -1);
+  stepperId += 0xe0;  // e0 is the 0th stepper on this uart port
+  uint8_t length = 3;
+  byte message[length];
+  message[0] = stepperId;
+  message[1] = commandId;
+  message[2] = calculateChecksum(message, length-1);
+  int bytes_written = port_->write(message, length);
+  if (bytes_written == length) {
+    Serial.println("Failed to send");
+    return false;
+  }
+  return true;
 }
 
-int MKS_SERVO42::reciveStepperStatus()
-{
-	int messageSize = 2 + sizeof(uint8_t);
-	byte receivedBytes[messageSize];
-	size_t rd = port_->readBytes(receivedBytes, messageSize);
-	for (size_t i = 0; i < 2 + sizeof(uint8_t); i++)
-	{
-		Serial.print(" ");
-		Serial.print(receivedBytes[i], HEX);
-	}
-	Serial.println();
-	return receivedBytes[1] == 1;
+bool MKS_SERVO42::sendMessageUint8(byte stepperId, byte const &commandId, uint8_t value) {
+  // Flush
+  while (port_->read() != -1);
+  stepperId += 0xe0;
+  uint8_t length = 4;
+  byte message[length];
+  message[0] = stepperId;
+  message[1] = commandId;
+  message[2] = value;
+  message[3] = calculateChecksum(message, length-1);
+  int bytes_written = port_->write(message, length);
+  if (bytes_written == length) {
+    Serial.println("Failed to send");
+    return false;
+  }
+  return true;
 }
 
-long MKS_SERVO42::getCurrentPosition(byte const &stepperId)
-{
-	// Flush
-	while (port_->read() != -1)
-		;
-	int send = sendMessage(stepperId, instruction::GET_ENCODER_POS);
-	if (send != 3)
-	{
-		Serial.println("Failed to send");
-		return -1;
-	}
-	return recieveEncoderPosition(stepperId);
+bool MKS_SERVO42::sendMessageUint16(byte stepperId, byte const &commandId, uint16_t value) {
+  // Flush
+  while (port_->read() != -1);
+  stepperId += 0xe0;
+  uint8_t length = 5;
+  byte message[length];
+  message[0] = stepperId;
+  message[1] = commandId;
+  message[2] = (value >> 8) & 0xFF;;
+  message[3] = value & 0xFF;
+  message[4] = calculateChecksum(message, length-1);
+  int bytes_written = port_->write(message, length);
+  if (bytes_written == length) {
+    Serial.println("Failed to send");
+    return false;
+  }
+  return true;
 }
 
-long MKS_SERVO42::recieveEncoderPosition(byte const &stepperId)
-{
-	byte receivedBytes[8] = {0xe0, 0x00, 0x00, 0x00, 0x00, 0x40, 0x20};
-	size_t bytesRead = port_->readBytes(receivedBytes, 8);
-	if (bytesRead == 8 && receivedBytes[0] == (stepperId + 0xE0))
-	{
-		int32_t carry = (int32_t)receivedBytes[1] << 24 | (int32_t)receivedBytes[2] << 16 | (int32_t)receivedBytes[3] << 8 | (int32_t)receivedBytes[4];
-		uint16_t value = (uint16_t)receivedBytes[5] << 8 | (uint16_t)receivedBytes[6];
-		return (carry * 0xffff) + value;
-	}
-	else
-	{
-		Serial.println("Invalid response from motor controller");
-		return false;
-	}
+uint8_t MKS_SERVO42::receiveUint8() {
+  int messageSize = 2 + sizeof(uint8_t);
+  byte bytes[messageSize];
+  if (port_->readBytes(bytes, messageSize) != messageSize) {
+    return 0; // Or some error value
+  }
+  return (uint8_t)bytes[1];
 }
 
-bool MKS_SERVO42::setTargetPosition(byte stepperId, byte direction, uint8_t speed, uint32_t pulses)
-{
-	if (speed > 2000 || direction > 1)
-	{
-		Serial.println("Speed out of range or invalid directionection");
-		return false;
-	}
-	stepperId += 0xE0;
-	byte message[8];
-	message[0] = stepperId;							// Slave address
-	message[1] = instruction::MOVE_SPEED_PULSES;	// Function code for running the motor
-	message[2] = (direction << 7) | (speed & 0x7F); // VAL byte, with directionection and speed
-	message[3] = (pulses >> 24) & 0xFF;
-	message[4] = (pulses >> 16) & 0xFF;
-	message[5] = (pulses >> 8) & 0xFF;
-	message[6] = pulses & 0xFF;
-	message[7] = calculateChecksum(message, 7);
-	port_->write(message, sizeof(message));
-
-	while (port_->read() != -1)
-		;
-	byte response[3];
-	size_t bytesRead = port_->readBytes(response, 3);
-	if (bytesRead == 3 && response[0] == stepperId)
-	{
-		if (response[1] == 0)
-		{
-			Serial.println("Run command failed");
-			return false;
-		}
-		else if (response[1] == 1)
-		{
-			Serial.println("Run command starting...");
-			return true;
-		}
-		else if (response[1] == 2)
-		{
-			Serial.println("Run command completed");
-			return true;
-		}
-	}
-	else
-	{
-		Serial.println("Invalid response from motor controller");
-		return false;
-	}
+uint16_t MKS_SERVO42::receiveUint16() {
+  int messageSize = 2 + sizeof(uint16_t);
+  byte bytes[messageSize];
+  if (port_->readBytes(bytes, messageSize) != messageSize) {
+    return 0; // Or some error value
+  }
+  return (uint16_t)(bytes[1] << 8) | bytes[2];
 }
 
-byte MKS_SERVO42::calculateChecksum(const byte *message, int length)
-{
-	byte checksum = 0;
-	for (int i = 0; i < length; i++)
-		checksum += message[i];
-	return checksum & 0xFF;
+uint32_t MKS_SERVO42::receiveUint32() {
+  int messageSize = 2 + sizeof(uint16_t);
+  byte bytes[messageSize];
+  if (port_->readBytes(bytes, messageSize) != messageSize) {
+    return 0; // Or some error value
+  }
+  return (uint32_t)bytes[0] << 24 | (uint32_t)bytes[1] << 16 | (uint32_t)bytes[2] << 8 | bytes[3];
+}
+
+// generic handler for any command that sends a number, and receives a status
+bool MKS_SERVO42::sendCommandAndCheckStatus(byte stepperId, byte commandId, uint16_t value) {
+  if (value == 0) {
+    if (!sendMessage(stepperId, commandId)) {
+      return false; // Failed to send message
+    }
+  } else if (value <= 255) {
+    if (!sendMessageUint8(stepperId, commandId, (uint8_t)value)) {
+      return false; // Failed to send message
+    }
+  } else {
+    if (!sendMessageUint16(stepperId, commandId, value)) {
+      return false; // Failed to send message
+    }
+  }
+
+  return receiveUint8() == 1;
+}
+
+bool MKS_SERVO42::ping(byte const &stepperId) {
+  // even though ping can return three possible statuses, we only care if it equals 1,
+  // so we can use the generic method.
+  return sendCommandAndCheckStatus(stepperId, instruction::STEPPER_PING);
+}
+
+// long MKS_SERVO42::getCurrentPosition(byte const &stepperId) {
+//   if (!sendMessage(stepperId, instruction::GET_ENCODER_POS)) {
+//     return -1;
+//   }
+//   return recieveEncoderPosition(stepperId);
+// }
+
+// long MKS_SERVO42::recieveEncoderPosition(byte const &stepperId) {
+//   byte receivedBytes[8] = {0xe0, 0x00, 0x00, 0x00, 0x00, 0x40, 0x20};
+//   size_t bytesRead = port_->readBytes(receivedBytes, 8);
+//   if (bytesRead == 8 && receivedBytes[0] == (stepperId + 0xE0)) {
+//     int32_t carry = (int32_t)receivedBytes[1] << 24 |
+//                     (int32_t)receivedBytes[2] << 16 |
+//                     (int32_t)receivedBytes[3] << 8 | (int32_t)receivedBytes[4];
+//     uint16_t value =
+//         (uint16_t)receivedBytes[5] << 8 | (uint16_t)receivedBytes[6];
+//     return (carry * 0xffff) + value;
+//   } else {
+//     Serial.println("Invalid response from motor controller");
+//     return false;
+//   }
+// }
+
+uint16_t MKS_SERVO42::getEncoderPosition(byte stepperId) {
+  if (!sendMessage(stepperId, instruction::GET_ENCODER_POS)) return 0;
+  return receiveUint16();
+}
+
+uint32_t MKS_SERVO42::getPulsesReceived(byte stepperId) {
+  if (!sendMessage(stepperId, instruction::READ_PULSES)) return 0;
+  return receiveUint32();
+}
+
+uint32_t MKS_SERVO42::getMotorShaftAngle(byte stepperId) {
+  if (!sendMessage(stepperId, instruction::READ_ANGLE)) return 0;
+  return receiveUint32();
+}
+
+int16_t MKS_SERVO42::getMotorShaftAngleError(byte stepperId) {
+  if (!sendMessage(stepperId, instruction::READ_ANGLE_ERROR)) return 0;
+  return (int16_t)receiveUint16(); // Cast to int16_t
+}
+
+bool MKS_SERVO42::runMotorConstantSpeed(byte stepperId, int8_t speed) {
+  // The int8_t here will have the sign bit int he right spot for the command
+  // arduino won't let me reinterpret_cast it go figure
+  while (port_->read() != -1);
+  stepperId += 0xe0;
+  uint8_t length = 4;
+  byte message[length];
+  message[0] = stepperId;
+  message[1] = instruction::RUN_MOTOR_CONSTANT_SPEED;
+  message[2] = speed;
+  message[3] = calculateChecksum(message, length-1);
+  int bytes_written = port_->write(message, length);
+  if (bytes_written == length) {
+    Serial.println("Failed to send");
+    return false;
+  }
+  return true;
+  return receiveUint8() == 1;
+}
+
+bool MKS_SERVO42::setTargetPosition(byte stepperId, int8_t speed, uint32_t pulses) {
+  if (abs(speed) > 2000) {
+    Serial.println("Speed out of range or invalid directionection");
+    return false;
+  }
+  stepperId += 0xE0;
+  byte message[8];
+  message[0] = stepperId;
+  message[1] = instruction::MOVE_SPEED_PULSES;
+  message[2] = speed;  // signed int gets turned into a byte. MSB indicates direction 
+  message[3] = (pulses >> 24) & 0xFF;
+  message[4] = (pulses >> 16) & 0xFF;
+  message[5] = (pulses >> 8) & 0xFF;
+  message[6] = pulses & 0xFF;
+  message[7] = calculateChecksum(message, 7);
+  port_->write(message, sizeof(message));
+
+  return receiveUint8() != 0;
+}
+
+// Implementations for commands that take a number and return a status
+bool MKS_SERVO42::setMotorType(byte stepperId, uint8_t motorType) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_MOTOR_TYPE, motorType);
+}
+
+bool MKS_SERVO42::setWorkMode(byte stepperId, uint8_t workMode) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_WORK_MODE, workMode);
+}
+
+bool MKS_SERVO42::setCurrentGear(byte stepperId, uint8_t currentGear) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_CURRENT_GEAR, currentGear);
+}
+
+bool MKS_SERVO42::setSubdivision(byte stepperId, uint16_t subdivision) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_SUBDIVISION, subdivision);
+}
+
+bool MKS_SERVO42::setEnPinActive(byte stepperId, uint8_t enPinActive) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_EN_PIN_ACTIVE, enPinActive);
+}
+
+bool MKS_SERVO42::setRotationDirection(byte stepperId, uint8_t rotationDirection) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_ROTATION_DIRECTION, rotationDirection);
+}
+
+bool MKS_SERVO42::setAutoSDD(byte stepperId, uint8_t autoSDD) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_AUTO_SDD, autoSDD);
+}
+
+bool MKS_SERVO42::setStallProtection(byte stepperId, uint8_t stallProtection) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_STALL_PROTECTION, stallProtection);
+}
+
+bool MKS_SERVO42::setSubdivisionInterpolation(byte stepperId, uint8_t subdivisionInterpolation) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_SUBDIVISION_INTERPOLATION, subdivisionInterpolation);
+}
+
+bool MKS_SERVO42::setBaudRate(byte stepperId, uint8_t baudRate) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_BAUD_RATE, baudRate);
+}
+
+bool MKS_SERVO42::setUartAddress(byte stepperId, uint8_t uartAddress) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_UART_ADDRESS, uartAddress);
+}
+
+bool MKS_SERVO42::setZeroMode(byte stepperId, uint8_t zeroMode) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_ZERO_MODE, zeroMode);
+}
+
+bool MKS_SERVO42::setZeroPosition(byte stepperId) { // No argument
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_ZERO_POSITION);
+}
+
+bool MKS_SERVO42::setZeroSpeed(byte stepperId, uint8_t zeroSpeed) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_ZERO_SPEED, zeroSpeed);
+}
+
+bool MKS_SERVO42::setZeroDirection(byte stepperId, uint8_t zeroDirection) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_ZERO_DIRECTION, zeroDirection);
+}
+
+bool MKS_SERVO42::returnToZero(byte stepperId) { // No argument
+  return sendCommandAndCheckStatus(stepperId, instruction::RETURN_TO_ZERO);
+}
+
+bool MKS_SERVO42::setPostionKp(byte stepperId, uint16_t kp) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_POSITION_KP, kp);
+}
+
+bool MKS_SERVO42::setPositionKi(byte stepperId, uint16_t ki) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_POSITION_KI, ki);
+}
+
+bool MKS_SERVO42::setPositionKd(byte stepperId, uint16_t kd) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_POSITION_KD, kd);
+}
+
+bool MKS_SERVO42::setAcceleration(byte stepperId, uint16_t acceleration) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_ACCELERATION, acceleration);
+}
+
+bool MKS_SERVO42::setMaxTorque(byte stepperId, uint16_t maxTorque) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_MAX_TORQUE, maxTorque);
+}
+
+bool MKS_SERVO42::restoreDefaultParameters(byte stepperId) {
+  return sendCommandAndCheckStatus(stepperId, instruction::RESTORE_DEFAULT_PARAMETERS);
+}
+
+bool MKS_SERVO42::setEnPinCrUart(byte stepperId, uint8_t enPinStatus) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SET_EN_PIN_CR_UART, enPinStatus);
+}
+
+bool MKS_SERVO42::stopMotor(byte stepperId) {
+  return sendCommandAndCheckStatus(stepperId, instruction::STOP_MOTOR);
+}
+
+bool MKS_SERVO42::saveClearStatus(byte stepperId, uint8_t status) {
+  return sendCommandAndCheckStatus(stepperId, instruction::SAVE_CLEAR_STATUS, status);
 }
--- src/MKS_SERVO42.h
+++ src/MKS_SERVO42.h
@@ -2,58 +2,131 @@
 /// \brief Provides control over MKS SERVO42 stepper motors via serial communication.
 ///
 /// \details This class is designed to interface with MKS SERVO42 stepper motors, allowing for
-///  		 initialization and control of the motors through a series of commands sent over a
-///  		 serial connection. It provides methods to ping motors to check their status, retrieve
-///  		 current position, and set target positions with specified speed and direction.
-///  		 It encapsulates the communication protocol needed to interact with the motors, providing
-///  		 a higher-level interface for ease of use in applications.
+///      initialization and control of the motors through a series of commands sent over a
+///      serial connection. It provides methods to ping motors to check their status, retrieve
+///      current position, and set target positions with specified speed and direction.
+///      It encapsulates the communication protocol needed to interact with the motors, providing
+///      a higher-level interface for ease of use in applications.
 #ifndef MKS_SERVO42_h
 #define MKS_SERVO42_h
 
 #include <Arduino.h>
 
-namespace instruction
-{
-	byte const GET_ENCODER_POS = 0x30;
-	byte const STEPPER_PING = 0x3A;
-	byte const MOVE_SPEED_PULSES = 0xFD;
+namespace instruction {
+  // read parameter commands
+  byte const GET_ENCODER_POS = 0x30;      // Read the encoder value
+  byte const READ_PULSES = 0x33;          // Read the number of pulses received
+  byte const READ_ANGLE = 0x36;           // Read the angle of the motor shaft
+  byte const READ_ANGLE_ERROR = 0x39;      // Read the error of the motor shaft angle
+  byte const STEPPER_PING = 0x3A;        // Read the En pins status, also knows as ping command
+  byte const READ_MOTOR_STATUS = 0x3E;     // Read the motor shaft status
+
+  // set system parameter commands
+  byte const CALIBRATE_ENCODER = 0x80;    // Calibrate the encoder
+  byte const SET_MOTOR_TYPE = 0x81;        // Set the motor type
+  byte const SET_WORK_MODE = 0x82;        // Set the work mode
+  byte const SET_CURRENT_GEAR = 0x83;      // Set the current gear
+  byte const SET_SUBDIVISION = 0x84;       // Set subdivision
+  byte const SET_EN_PIN_ACTIVE = 0x85;     // Set the active of the En pin
+  byte const SET_ROTATION_DIRECTION = 0x86; // Set the direction of motor rotation
+  byte const SET_AUTO_SDD = 0x87;          // Set automatic turn off the screen
+  byte const SET_STALL_PROTECTION = 0x88;   // Set the stall protection
+  byte const SET_SUBDIVISION_INTERPOLATION = 0x89; // Set the subdivision interpolation function
+  byte const SET_BAUD_RATE = 0x8A;         // Set the baud rate
+  byte const SET_UART_ADDRESS = 0x8B;      // Set the uart address
+
+  // set zeroMode commands
+  byte const SET_ZERO_MODE = 0x90;         // Set the mode of zeroMode
+  byte const SET_ZERO_POSITION = 0x91;     // Set the zero of zeroMode
+  byte const SET_ZERO_SPEED = 0x92;        // Set the speed of zeroMode
+  byte const SET_ZERO_DIRECTION = 0x93;    // Set the dir of zeroMode
+  byte const RETURN_TO_ZERO = 0x94;        // Return to zero
+
+  // Se PID, Acceleration, and Torque commands
+  byte const SET_POSITION_KP = 0xA1;        // Set the position Kp parameter
+  byte const SET_POSITION_KI = 0xA2;        // Set the position Ki parameter
+  byte const SET_POSITION_KD = 0xA3;        // Set the position Kd parameter
+  byte const SET_ACCELERATION = 0xA4;       // Set the acceleration (ACC) parameter
+  byte const SET_MAX_TORQUE = 0xA5;         // Set the maximum torque (MaxT) parameter
+
+  // Restore defaults
+  byte const RESTORE_DEFAULT_PARAMETERS = 0x3F; // Restore default parameter command
+
+  // Serial control
+  byte const SET_EN_PIN_CR_UART = 0xF3;     // Set the En pin status in CR_UART mode
+  byte const RUN_MOTOR_CONSTANT_SPEED = 0xF6; // Run the motor forward / reverse in a Constant speed
+  byte const STOP_MOTOR = 0xF7;            // Stop the motor
+  byte const SAVE_CLEAR_STATUS = 0xFF;      // Save/Clear the status set by the RUN_MOTOR_CONSTANT_SPEED command
+  byte const MOVE_SPEED_PULSES = 0xFD;     // Run the motor to an angle at a speed. This can be more than one rotation away
 };
 
 /// @brief Driver for MKS SERVO42 stepper motors via serial communication
-class MKS_SERVO42
-{
-public:
-	/// @brief Initializes the MKS_SERVO42 object with a serial port and a baud rate.
-	/// @param serialPort Pointer to the HardwareSerial object that corresponds to the serial port.
-	/// @param baudRate The baud rate for serial communication. If not specified, defaults to 38400.
-	void initialize(HardwareSerial *serialPort = nullptr, long const &baudRate = 38400);
+/// original doumentation of serial protocol https://github.com/makerbase-mks/MKS-SERVO42C/wiki/Serial-communication-description
+class MKS_SERVO42 {
+ public:
+  /// @brief Initializes the MKS_SERVO42 object with a serial port and a baud rate.
+  /// @param serialPort Pointer to the HardwareSerial object that corresponds to the serial port.
+  /// @param baudRate The baud rate for serial communication. If not specified, defaults to 38400.
+  void initialize(HardwareSerial *serialPort = nullptr, long const &baudRate = 38400);
 
-	/// @brief Sends a ping command to the stepper motor to check its presence and connectivity.
-	/// @param stepperId The ID of the stepper motor to ping.
-	/// @return Returns true if the stepper motor is successfully pinged, false otherwise.
-	bool ping(byte const &stepperId);
+  /// @brief Sends a ping command to the stepper motor to check its presence and connectivity.
+  /// @param stepperId The ID of the stepper motor to ping.
+  /// @return Returns true if the stepper motor is successfully pinged, false otherwise.
+  bool ping(byte const &stepperId);
 
-	/// @brief Retrieves the current position of the stepper motor in pulses.
-	/// @param stepperId The ID of the stepper motor whose position is to be retrieved.
-	/// @return Returns the current position of the stepper motor as a long integer value.
-	long getCurrentPosition(byte const &stepperId);
+  uint16_t getEncoderPosition(byte stepperId);
+  uint32_t getPulsesReceived(byte stepperId);
+  uint32_t getMotorShaftAngle(byte stepperId);
+  int16_t getMotorShaftAngleError(byte stepperId);
 
-	/// @brief Sets the target position for the stepper motor by specifying the direction, speed, and number of pulses.
-	///        The speed parameter should not exceed 2000 RPM and the direction should be 0 for one way or 1 for the opposite.
-	/// @param stepperId The ID of the stepper motor to which the command will be sent.
-	/// @param direction The direction in which the motor should run (0 or 1).
-	/// @param speed The speed at which the motor should run, up to a maximum of 2000 RPM.
-	/// @param pulses The number of pulses to move, which translates to the target position.
-	/// @return Returns true if the command is successfully sent and the motor starts running, false if there is an error.
-	bool setTargetPosition(byte stepperId, byte direction, uint8_t speed, uint32_t pulses);
+  /// @brief Sets the target position for the stepper motor by specifying the direction, speed, and number of pulses.
+  ///        The speed parameter should not exceed 2000 RPM and the direction should be 0 for one way or 1 for the opposite.
+  /// @param stepperId The ID of the stepper motor to which the command will be sent.
+  /// @param speed The speed at which the motor should run, up to a maximum of 2000 RPM. negative numbers spin the other direction
+  /// @param pulses The number of pulses to move, which translates to the target position.
+  /// @return Returns true if the command is successfully sent and the motor starts running, false if there is an error.
+  bool setTargetPosition(byte stepperId, int8_t speed, uint32_t pulses);
 
-private:
-	HardwareSerial *port_;
+  bool runMotorConstantSpeed(byte stepperId, int8_t speed);
 
-	int sendMessage(byte stepperId, byte const &commandID);
-	int reciveStepperStatus();
-	long recieveEncoderPosition(byte const &stepperId);
-	byte calculateChecksum(const byte *message, int length);
+  bool setMotorType(byte stepperId, uint8_t motorType);
+  bool setWorkMode(byte stepperId, uint8_t workMode);
+  bool setCurrentGear(byte stepperId, uint8_t currentGear);
+  bool setSubdivision(byte stepperId, uint16_t subdivision);
+  bool setEnPinActive(byte stepperId, uint8_t enPinActive);
+  bool setRotationDirection(byte stepperId, uint8_t rotationDirection);
+  bool setAutoSDD(byte stepperId, uint8_t autoSDD);
+  bool setStallProtection(byte stepperId, uint8_t stallProtection);
+  bool setSubdivisionInterpolation(byte stepperId, uint8_t subdivisionInterpolation);
+  bool setBaudRate(byte stepperId, uint8_t baudRate);
+  bool setUartAddress(byte stepperId, uint8_t uartAddress);
+  bool setZeroMode(byte stepperId, uint8_t zeroMode);
+  bool setZeroPosition(byte stepperId);
+  bool setZeroSpeed(byte stepperId, uint8_t zeroSpeed);
+  bool setZeroDirection(byte stepperId, uint8_t zeroDirection);
+  bool returnToZero(byte stepperId);
+  bool setPostionKp(byte stepperId, uint16_t kp);
+  bool setPositionKi(byte stepperId, uint16_t ki);
+  bool setPositionKd(byte stepperId, uint16_t kd);
+  bool setAcceleration(byte stepperId, uint16_t acceleration); // if you set this too high you will let the magic smoke out
+  bool setMaxTorque(byte stepperId, uint16_t maxTorque);
+  bool restoreDefaultParameters(byte stepperId);
+  bool setEnPinCrUart(byte stepperId, uint8_t enPinStatus);
+  bool stopMotor(byte stepperId);
+  bool saveClearStatus(byte stepperId, uint8_t status); // resets the runMotorConstantSpeed command
+
+ private:
+  HardwareSerial *port_;
+
+  bool sendMessage(byte stepperId, byte const &commandID);
+  bool sendMessageUint8(byte stepperId, byte const &commandId, uint8_t value);
+  bool sendMessageUint16(byte stepperId, byte const &commandId, uint16_t value);
+  uint8_t receiveUint8();
+  uint16_t receiveUint16();
+  uint32_t receiveUint32();
+  bool sendCommandAndCheckStatus(byte stepperId, byte commandId, uint16_t value = 0);
+  long recieveEncoderPosition(byte const &stepperId);
+  byte calculateChecksum(const byte *message, int length);
 };
 
 #endif
